171ee08f3cd0888d6284c53f8b8c5f1bcd855b60
Nayan Chaurasia <100986568+nayanchaurasia@users.noreply.github.com>
2022-12-24 13:55:36 +0530

    Add files via upload
    this is the program for led blinking  like the diwali lights

---
+++ led blink one by one as jhalar.ino
@@ -0,0 +1,31 @@
+int led1=13;
+int led2=12;
+int led3=11;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led1,OUTPUT);
+pinMode(led2,OUTPUT);
+pinMode(led3,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led1,LOW);
+digitalWrite(led2,LOW);
+digitalWrite(led3,LOW);
+
+digitalWrite(led1,HIGH);
+delay(40);
+digitalWrite(led1,LOW);
+delay(40);
+digitalWrite(led2,HIGH);
+delay(40);
+digitalWrite(led2,LOW);
+delay(40);
+digitalWrite(led3,HIGH);
+delay(40);
+digitalWrite(led3,LOW);
+delay(40);
+
+
+}
