d6fa210eacadac5a2d8582e12c7a6b40b1f1dcbd
Nayan Chaurasia <100986568+nayanchaurasia@users.noreply.github.com>
2023-01-11 18:11:28 +0530

    Add files via upload

---
+++ dimming led.ino
@@ -0,0 +1,19 @@
+int potpin=A1;
+int ledpin=11;
+int potval;
+int ledval;
+int dt=1000;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(potpin,INPUT);
+pinMode(ledpin,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+potval=analogRead(potpin);
+ledval=(255./1023.)*potval;
+analogWrite(ledpin,ledval);
+Serial.println(ledval);
+}
