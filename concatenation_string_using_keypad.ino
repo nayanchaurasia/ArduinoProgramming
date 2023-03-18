e8e39f6205bc5478fa853c946c5654b68df8471e
Nayan Chaurasia <100986568+nayanchaurasia@users.noreply.github.com>
2023-03-18 13:07:32 +0530

    Add files via upload

---
+++ concatenation_string_using_keypad.ino
@@ -0,0 +1,48 @@
+#include<Keypad.h>
+const byte rows=4;
+const byte col=4;
+
+int alert=13;
+
+char keys[rows][col]={
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}, 
+  };
+
+String pass="ABCD";
+
+byte rowPin[rows]={2,3,4,5};
+byte colPin[col]={6,7,8,9};
+
+String text="";
+
+Keypad password =Keypad(makeKeymap(keys),rowPin,colPin,rows,col);
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(alert,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+char value=password.getKey();
+if (value != NO_KEY){
+  Serial.println(value);
+  if (value=='*'){
+    text="";
+  }
+  else{
+    if (value=='#'){
+       Serial.println("String is ");
+       Serial.println(text);
+       text="";   
+      }
+    else{
+      text+=value;
+    }
+  }
+}
+}
